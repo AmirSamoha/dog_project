8ae882ef5ea9cd0df2c1be25b405636feb35b500
Amir Samoha <amir.samoha42@gmail.com>
2023-01-09 12:21:47 +0200

    update 9.1.22

---
+++ doginfo/log_register/register.js
@@ -0,0 +1,25 @@
+async function login_data() {
+    const response = await fetch("data.json");
+    const data = await response.json();
+  
+    const userName_val = document.getElementById("user").value;
+    const userName_password_val = document.getElementById("pass").value;
+
+    for (let key in data) {
+      for (let i in data[key]) {
+        let userName = data[key][i].username;
+        let userName_password = data[key][i].password;
+
+        if (userName === userName_val && userName_password === userName_password_val){
+          alert("Username exists");
+          sessionStorage.setItem("login_active", true);
+          sessionStorage.setItem(
+            "text_value",
+            document.getElementById("user").value);
+          location.assign("../index.html");
+          
+        }
+      }
+    }
+
+}
--- doginfo/script.js
+++ doginfo/script.js
@@ -1,20 +1,39 @@
-let nameVal;
-let passwordVal;
-let rePasswordVal;
-let fnameVal;
-let lnameVal;
-function check(){
-    nameVal = document.getElementById("uname").value;
-    fnameVal = document.getElementById("fname").value;
-    lnameVal = document.getElementById("lname").value;
-    passwordVal = document.getElementById("pwd").value;
-    rePasswordVal = document.getElementById("repwd").value;
 
-    if(passwordVal == rePasswordVal){
-        alert(nameVal + " Login success!" )
-    }else{
-        alert("Passwords do not match!Try again")
+
+
+function checkConnection() {
+    //פונקציה לבדיקה האם המשתמש מחובר לדף זה
+    if (
+      sessionStorage.getItem("login_active") === "true"
+    ) {
+    } else if (
+      sessionStorage.getItem("login_active") !== "true"
+    ) {
+      window.location = ("../doginfo/log_register/index.html");
     }
 }
+setInterval(checkConnection, 500);
 
 
+const redirect = (link) => {
+  // פונקציה לשינוי מיקום לאחר לחיצה
+  window.location.assign(link);
+};
+
+function deleteItems() {
+  // פונקציה למחיקת נתונים מ-sessionStorage
+  sessionStorage.clear();
+}
+
+document.getElementById("userName").textContent = sessionStorage.getItem("text_value");
+
+if (sessionStorage.getItem("login_active") === "true"){
+  document.getElementById("btn_log_sign").style.display = "none";
+
+  $(document).ready(function () {
+    $(".navbar").prepend(`<button class="btn btn-primary me-md-2" type="button" id="btn_logout" onclick="deleteItems(); redirect('log_register/index.html')"> Log Out </button>`)
+    $("#btn_logout").css({"font-size": "12px"})
+  });
+
+
+}
